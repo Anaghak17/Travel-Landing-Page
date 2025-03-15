37e2f75b2dcb10ab1b3db516242b4fa365f23eb4
anagha <anagha1720@gmai.com>
2025-03-15 16:51:44 +0530

    website

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// Function to load external HTML files
-// function loadHTML(id, file) {
-//     fetch(file)
-//         .then(response => response.text())
-//         .then(data => document.getElementById(id).innerHTML = data)
-//         .catch(error => console.error('Error loading file:', error));
-// }
-
-// // Load header and footer when the page loads
-// document.addEventListener("DOMContentLoaded", function () {
-//     loadHTML("header", "header.html");
-//     loadHTML("footer", "footer.html");
-// });
 function loadComponent(file, elementId, callback) {
     fetch(file)
         .then(response => response.text())
